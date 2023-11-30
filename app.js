76603a8abdfa7a536e49d7c4ee25178768e0a2d8
muhammad45401 <saadjani3333@gmail.com>
2023-12-01 01:32:08 +0500

    Math mathods

---
+++ app.js
@@ -0,0 +1,57 @@
+document.write("<h3> Q:-1 </h3>")
+
+
+var a = Math.round(3.45214)
+document.write(a);
+
+document.write("<br>")
+
+var a = Math.ceil(3.45214)
+document.write(a);
+
+
+document.write("<br>")
+document.write("<h3> Q:-2 </h3>")
+
+
+//  var b = prompt("Enter the negative value");
+//  document.write("<h3> Number </h3>" +"<br>"+ b);
+
+
+// document.write("<br> <h4> Round </h4>")
+//  document.write(Math.round(b));
+
+ 
+// document.write("<br> <h4> floor </h4>")
+// document.write(Math.floor(b));
+
+// document.write("<br> <h4> floor </h4>")
+// document.write(Math.ceil(b));
+
+
+
+document.write("<h3> Q:-3 </h3>")
+
+
+var c = Math.abs(-4)
+document.write(" The absolute value of  ")
+document.write(c);
+
+
+document.write("<h3> Q:-4 </h3>")
+
+
+var d = Math.floor(Math.random () *10)+1;
+document.write(d)
+document.write("<br>");
+
+var e = Math.floor(Math.random () *10)+1;
+document.write(e)
+
+
+
+
+
+
+
+
