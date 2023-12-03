10e256353e5242db20792a1fa98786e172c01eaf
muhammad45401 <saadjani3333@gmail.com>
2023-12-04 01:54:46 +0500

    math update

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ document.write("<h3> Q:-3 </h3>")
 
 
 var c = Math.abs(-4)
-document.write(" The absolute value of  ")
+document.write("The absolute value of " )
 document.write(c);
 
 
@@ -42,11 +42,71 @@ document.write("<h3> Q:-4 </h3>")
 
 
 var d = Math.floor(Math.random () *10)+1;
-document.write(d)
+document.write(" random dice value : "+d)
 document.write("<br>");
 
 var e = Math.floor(Math.random () *10)+1;
-document.write(e)
+document.write(" random dice value : "+e);
+
+// Q:- 5
+
+document.write("<br>");
+document.write("<h3> Q:-5 </h3>")
+
+
+var rnd = Math.floor(Math.random()*2)
+
+
+
+if ( rnd === 1){
+      document.write( "<h4> HEADS </h4>")
+    document.write( "you win")
+
+}else{
+     document.write( "<h4> TAILS </h4>")
+    document.write("you loos")
+};
+
+// Q:- 6
+
+document.write("<br>");
+document.write("<h3> Q:-6 </h3>")
+
+var e = Math.floor(Math.random () *100)+1;
+document.write(" random number between 1 and 100 : " + e);
+
+// Q:- 7
+
+document.write("<br>");
+document.write("<h3> Q:-7 </h3>")
+
+// var weight = prompt(" Enter your waight");
+
+// for ( var i=0; i<=weight.length; i++){
+//     if ( weight.slice(i,i+weight.length)=== weight){
+//         document.write( "The weight is user"+" "+ weight+" "+"Kilograms")
+//     }
+// }
+
+
+// Q:- 8
+document.write("<br>");
+document.write("<h3> Q:-8 </h3>");
+
+
+var rnd = Math.floor(Math.random()*10)+1;
+var chek = prompt("this is a game \n plase Enter a number");
+
+if( rnd === chek){
+    alert("<h5> Congratulation<h5>")
+    document.write(" <h5> Congratulation<h5>")
+
+}else{
+    alert("try agen")
+}
+
+
+
 
 
 
